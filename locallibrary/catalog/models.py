edb0b2f71df9b54e77e25001cfc7e3591a3f2d41
Andy Dearing <andy.dearing@gmail.com>
2019-06-18 09:00:25 -0400

    finished Chapter 4

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 class Genre(models.Model):
     """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text=
-    'Enter a book genre (e.g. Science Fiction)')
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
-def __str__(self):
-    """String for representing the Model object."""
-    return self.name
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
 
 from django.urls import reverse # Used to genertae URLs by reversing
 # the URL patterns
@@ -46,6 +45,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book"""
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """create a string for the Genre. This is required to
+        display genre in Admin"""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 import uuid # required for book instances
 
