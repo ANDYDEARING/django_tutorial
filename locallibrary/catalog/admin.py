f2318b1e874512932454ca544cf6aac3d72a6927
Andy Dearing <andy.dearing@gmail.com>
2019-06-18 14:13:12 -0400

    finished Chapter 4 for real

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -8,10 +8,14 @@ from catalog.models import Author, Genre, Book, BookInstance
 admin.site.register(Genre)
 # admin.site.register(BookInstance)
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
 # define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -29,7 +33,7 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-
+    list_display = ('book', 'status', 'due_back', 'id')
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
