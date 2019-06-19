c6a3ce212ab73b0f1e1df12807353a1cb029a3c0
Andy Dearing <andy.dearing@gmail.com>
2019-06-19 09:12:16 -0400

    got author list, working on author detail

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -42,12 +42,6 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    # your own name for the list as a template variable
-    # context_object_name = 'my_book_list'
-    # get 5 books containing the title war
-    # queryset = Book.objects.filter(title__icontains='war')[:5]
-    # # specify your own template name/location
-    # template_name = 'books/my_arbitrary_template_name_list.html'
     
 class BookDetailView(generic.DetailView):
     model = Book
@@ -55,7 +49,7 @@ class BookDetailView(generic.DetailView):
 class AuthorListView(generic.ListView):
     model = Author
 
-class AuthorDetailView(generic.ListView):
+class AuthorDetailView(generic.DetailView):
     model = Author
 
 
